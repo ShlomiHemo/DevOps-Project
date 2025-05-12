ff35540a0e1d87939ddce5ea672f1f8da2785aaf
shlomi hemo <shlomihemo82@gmail.com>
2025-05-12 20:16:49 +0300

    changes

---
+++ src/main/java/com/shlomi/filter/HeaderFilter.java
@@ -0,0 +1,29 @@
+package com.shlomi.filter;
+
+import javax.servlet.*;
+import javax.servlet.annotation.WebFilter;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebFilter("/*")
+public class HeaderFilter implements Filter {
+    
+    @Override
+    public void init(FilterConfig filterConfig) throws ServletException {
+        // Initialization code if needed
+    }
+    
+    @Override
+    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
+            throws IOException, ServletException {
+        HttpServletResponse httpResponse = (HttpServletResponse) response;
+        httpResponse.setHeader("skip_zrok_interstitial", "true");
+        
+        chain.doFilter(request, response);
+    }
+    
+    @Override
+    public void destroy() {
+        // Cleanup code if needed
+    }
+}
